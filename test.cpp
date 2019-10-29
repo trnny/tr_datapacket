64edecd552f238c84e01b7ac5899aa5b1162c82f
trnny <thuawu27@gmail.com>
2019-10-29 14:06:48 +0800

    模拟一下文件请求和应答过程

--- test.cpp
+++ test.cpp
@@ -1,49 +1,71 @@
 #include "datapacket.h"
 #include <iomanip>
+#include <map>
+
+_byte a[1024];          // 模拟双方收到的数据包
+int pl;                 // 模拟双方收到的数据包长度
+/**
+ * 模拟获得消息
+ */
+bool _f = true;
+
+bool ___p(){
+    // 模拟产生请求
+    
+}
+void ___v(){
+    // 模拟处理请求
+    char desc[40];
+    tr_field tr(a, pl); // 收到的请求
+    tr.get(desc, 0);
+    string str(desc);
+    if(str == "请求文件准备"){              // name
+        char fn[40];
+        tr.get(0, fn);
+        int fnl = strlen(fn) + 1;
+        int fsize, fid;
+        tr_field trb("应答文件准备", 300);  // id/size/name
+        // ... 假设这里找到了文件
+        if(_f){
+            fsize = 100, fid = 123;
+            cout << "请求文件(找到): " << fn << ",size: " << fsize << ", id: " << fid << endl;
+        }
+        // ... 假设这里没找到文件
+        else{
+            fsize = 0, fid = 0;
+            cout << "请求文件(未找到): " << fn << endl;
+        }
+        trb.push(&fid, 4);
+        trb.push(&fsize, 4);
+        trb.push(fn, fnl);
+        trb.packet(a, pl);
+    }
+    else if(str == "请求文件中"){           // id/beg
+        int fid, beg;
+        tr.getd(&fid, 4, 0);
+        tr.getd(&beg, 4, 4);
+        tr_field trb("应答文件中", 300);    // id/beg/len/data
+        trb.push(&fid, 4);
+        trb.push(&beg, 4);
+        // ... 假设这是传输的数据
+        char* rdata = "文件内容第一部分";
+        int len = strlen(rdata);
+        trb.push(&len, 4);
+        trb.push(rdata, len);
+        trb.packet(a, pl);
+    }
+    else if(str == "请求文件确认"){         // id/status
+        // ... 请求完成
+    }
+}
 
 int main(){
-    tr_field d1("ceshi1", 200);
-    tr_field d2 = d1;
-    char c[40], t[40];
-    d2.push("\0\0\1\0", 4);
-    d2.push("hello world", 12);
-    _byte p[100];
-    int l;
-    d2.packet(p, l);
-    tr_field d3(p, l);
-    d2.getd(0, &l, 4);
-    d3.getd(c, t, 12, 4);
-    cout << l << '\n' << c << " " << t << endl;
-    char *cz = "中文会咋样";
-    int czl = strlen(cz) + 1;
-    d3.push(cz, czl);
-    d3.get(0, p, l);
-    tr_field d4("测试2", p, l);
-    d4.getd(c, t, czl, 16);
-    cout << c << " " << t << endl;
-    progress pr;
-    pr.put(0, 23);
-    pr.put(45, 70);
-    pr.put(24, 30);
-    pr.put(23, 24);
-    cout << pr.prog() << " " << pr.size() << endl;
-    progress pr2(350);
-    pr2.put(100, 150);
-    progress pr3(pr2);
-    pr3.put(20, 80);
-    cout << pr2.size() << " " << pr3.size() << endl;
-    progress pr4 = pr3;
-    pr4.put(234, 333);
-    cout << pr2.size() << " " << pr3.size() << " " << pr4.size() << endl;
-    int beg, end;
-    if(pr4.block(beg, end))
-        cout << beg << " " << end << endl;
-    if(pr4.block(beg, end, 1))
-        cout << beg << " " << end << endl;
-    if(pr4.block(beg, end, 2))
-        cout << beg << " " << end << endl;
-    pr4.put(0, 20);
-    if(pr4.block(beg, end, 0))
-        cout << beg << " " << end << endl;
+    _byte b[1024];
+    datapacket(1024);
+    while (___p())
+    {
+        ___v();
+    }
+
     return 0;
 }
